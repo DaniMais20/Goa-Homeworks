f4ed6b873f04a504d2cc5672289c3c520eada778
DaniMais20 <maisuradzedani@gmail.com>
2026-07-14 19:00:25 +0400

    New homework

---
+++ Works/classworks/Work5.py
@@ -0,0 +1,8 @@
+number = int("30")
+print(type(number))
+
+number2 = str(20)
+print(type(number2))
+
+number3 = float(10)
+print(type(number3))
---
+++ Works/homeworks/homework4.py
@@ -0,0 +1,51 @@
+name = input("Enter your name: ") #Dani
+surname = input("Enter your surname: ") #maisuradze
+age = int(input("Enter your age: ")) # 17
+
+print("Hello my name is", name + ' ' "my surname is", surname, "and I am", age, "years old")
+
+
+
+
+num1 = float(input("Enter the first decimal number: "))
+num2 = float(input("Enter the second decimal number: "))
+
+print("Sum:", num1 + num2)
+print("Difference:", num1 - num2)
+print("Product:", num1 * num2)
+print("Quotient:", num1 / num2)
+
+
+
+
+
+text = "Python"
+number = 20
+decimal = 5.15
+is_weather = True
+user_input = input("Enter any value: ")
+
+print(type(text))
+print(type(number))
+print(type(decimal))
+print(type(is_weather))
+print(type(user_input))
+
+
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+favorite_number = float(input("Enter your favorite number: "))
+
+print("Name:", name, "-", type(name))
+print("Age:", age, "-", type(age))
+print("Favorite number:", favorite_number, "-", type(favorite_number))
+
+
+
+
+word = input("Enter a word: ")
+number = int(input("Enter a number: "))
+
+print("You entered the word:", word, "and the number:", number)
+
