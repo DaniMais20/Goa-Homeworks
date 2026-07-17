e1cb5c279110bd9dd99247a470149b7507562fab
DaniMais20 <maisuradzedani@gmail.com>
2026-07-17 12:49:23 +0400

    new

--- Works/homeworks/homework5.py
+++ Works/homeworks/homework5.py
@@ -1,23 +1,23 @@
-name = input("Enter your name: ")
-age = int(input("Enter your age: "))
-height = float(input("Enter your height in cm: "))
+name = input("Enter your name: ")#dani
+age = int(input("Enter your age: "))#17
+height = float(input("Enter your height in cm: "))#180
 
 print(name, age, height)
 
 print(age >= 18 and height > 170)
 
-number1 = int(input("Enter the first number: "))
-number2 = int(input("Enter the second number: "))
+number1 = int(input("enter the first number: "))
+number2 = int(input("enter the second number: "))
 
 print(number1 > 0 or number2 > 0)
 
-favorite_color = input("Enter your favorite color: ")
-age = int(input("Enter your age: "))
+favorite_color = input("enter your favorite color: ")
+age = int(input("enter your age: "))
 
-print(favorite_color == "blue" or age < 18)
+print(favorite_color == "red" or age < 18)
 
-name = input("Enter your name: ")
-balance = float(input("Enter your balance: "))
-vip = input("Are you a VIP? (True or False): ") == "True"
+name = input("enter your name: ")
+balance = float(input("enter your balance: "))
+vip = input("are you a vip? (True or False): ") == "true"
 
 print(balance > 100 or vip)
